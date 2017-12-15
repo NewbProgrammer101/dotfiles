ba5e0dd36826de5d3783abc24de2fc2f7f223bfb
Jalus Bilieyich <countolaf17@gmail.com>
2017-12-14 18:38:54 -0600

    new file:   .scripts/.games 	new file:   .scripts/system-resources.sh

---
+++ .scripts/system-resources.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "CPU Information:"
+lscpu
+echo
+echo "Partitions:"
+lsblk
+echo
+echo "IPC information:"
+lsipc
+echo
+echo "USB info:"
+lsusb -v
+echo
+echo "PCI devices:"
+sudo lspci -nnk
+echo
+echo "Login Information of Users:"
+lslogins 
+echo
+lslocks
+echo
+echo "Other info:"
+cat /proc/cmdline
+uname -s -n -r -m -i -o
+cat /proc/consoles
+cat /proc/filesystems
+echo
+echo "System Framebuffer:"; cat /proc/fb
+echo
+echo "RTC Info:"; cat /proc/driver/rtc
+
