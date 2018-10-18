99789705ca7a54dc25f3be52251a18fc24ffddb4
Jalus Bilieyich <countolaf17@gmail.com>
2018-10-17 21:35:14 -0500

    Still broken

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@
 INPUT=$1
 
 function install_dotfiles {
-  echo -ne "Copying custom files to home directory..."
+  echo "Copying custom files to home directory..."
   cp -rv home/.* $HOME/
   cp -rv home/misc/.* $HOME/
-  echo -ne "Copying files to system-wide config... (Needs password)"
+  echo "Copying files to system-wide config... (Needs password)"
   sudo cp -rv etc/* /etc/
 }
 
-echo "Are you sure you want to copy someone's personal configs to your setup? [Y/n]: "
+echo -ne "Are you sure you want to copy someone's personal configs to your setup? [Y/n]: "
 read $INPUT
 
 case $INPUT in
   [Y] ) install_dotfiles;;
-  [n] ) echo -ne "Ok. Good choice.";;
+  [n] ) echo "Ok. Good choice.";;
 esac
